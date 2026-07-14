a4d34fbf12b5e310f51206cf871f9775525db2ab
Mateo Tribules <mateo.tribules@akroscorp.com>
2026-07-13 23:56:52 -0500

    InventarioCOntroller

--- src/main/java/com/saludvida/app/controller/InventarioController.java
+++ src/main/java/com/saludvida/app/controller/InventarioController.java
@@ -1,5 +1,18 @@
 package com.saludvida.app.controller;
 
-public class InventarioController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+public class InventarioController {
+	
+	@Controller
+	@RequestMapping("inventario")
+	public class HomeController {
+		
+		@GetMapping
+		 public String index() {
+	        return "/inventario/categorias"; //ruta fisica de la carpeta
+	    }
+	}
 }
